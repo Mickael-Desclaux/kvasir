27c2c35212d92f94a45c5de36970f165e47b07e4
Mickael <mickaeldesclaux@gmail.com>
2026-03-07 08:24:11 +0100

    add hardcoded data for some greek gods

---
+++ hardcoded-data/greek-gods.ts
@@ -0,0 +1,194 @@
+export const GREEK_GODS = [
+	{
+		id: 1,
+		name: "Zeus",
+		pantheon: "Grec",
+		domains: [
+			"ciel",
+			"foudre",
+			"tonnerre",
+			"justice",
+			"ordre",
+			"souveraineté divine",
+		],
+		description:
+			"Zeus est le roi des dieux dans la mythologie grecque et la divinité suprême du panthéon olympien. Dieu du ciel et de la foudre, il gouverne les autres dieux et veille au respect de l'ordre, des lois et de la justice.",
+		symbols: ["foudre", "aigle", "chêne", "trône"],
+		attributes: ["foudre", "sceptre", "aigle", "égide"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Cronos", "Rhéa"],
+			},
+			{
+				type: "spouse",
+				entities: ["Héra"],
+			},
+			{
+				type: "siblings",
+				entities: ["Poséidon", "Hadès", "Déméter", "Hestia", "Héra"],
+			},
+			{
+				type: "children",
+				entities: [
+					"Athéna",
+					"Apollon",
+					"Artémis",
+					"Arès",
+					"Hermès",
+					"Dionysos",
+					"Héphaïstos",
+					"Perséphone",
+					"Héraclès",
+					"Hélène",
+					"Minos",
+				],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/Zeus"],
+	},
+	{
+		id: 2,
+		name: "Héra",
+		pantheon: "Grec",
+		domains: ["mariage", "famille", "femmes", "ciel"],
+		description:
+			"Déesse du mariage et reine des dieux, Héra est l'épouse de Zeus et la protectrice des femmes et de la vie conjugale. Fille de Cronos et Rhéa, elle règne sur l'Olympe aux côtés de Zeus et est connue pour sa jalousie envers les nombreuses conquêtes de son époux.",
+		symbols: ["paon", "coucou", "grenade", "diadème"],
+		attributes: ["sceptre", "diadème", "voile"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Cronos", "Rhéa"],
+			},
+			{
+				type: "spouse",
+				entities: ["Zeus"],
+			},
+			{
+				type: "siblings",
+				entities: ["Poséidon", "Hadès", "Déméter", "Hestia", "Zeus"],
+			},
+			{
+				type: "children",
+				entities: ["Hébé", "Arès", "Héphaïstos", "Eileithyia"],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/H%C3%A9ra"],
+	},
+	{
+		id: 3,
+		name: "Poséidon",
+		pantheon: "Grec",
+		domains: ["mer", "tremblements de terre", "chevaux", "tempêtes"],
+		description:
+			"Poséidon est le dieu des mers, des océans et des tremblements de terre. Frère de Zeus et d'Hadès, il règne sur les eaux du monde depuis son palais sous-marin et commande aux tempêtes, aux flots et aux chevaux.",
+		symbols: ["trident", "cheval", "dauphin", "taureau"],
+		attributes: ["trident", "couronne de corail"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Cronos", "Rhéa"],
+			},
+			{
+				type: "spouse",
+				entities: ["Amphitrite"],
+			},
+			{
+				type: "siblings",
+				entities: ["Zeus", "Hadès", "Déméter", "Hestia", "Héra"],
+			},
+			{
+				type: "children",
+				entities: ["Triton", "Polyphème", "Thésée"],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/Pos%C3%A9idon"],
+	},
+	{
+		id: 4,
+		name: "Athéna",
+		pantheon: "Grec",
+		domains: ["sagesse", "guerre", "artisanat", "justice", "stratégie"],
+		description:
+			"Athéna est la déesse de la sagesse, de la guerre stratégique et de l'artisanat. Née directement du crâne de Zeus tout armée, elle est la protectrice d'Athènes et incarne l'intelligence, la raison et la justice.",
+		symbols: ["chouette", "olivier", "lance", "égide"],
+		attributes: ["casque", "lance", "bouclier", "égide"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Zeus", "Métis"],
+			},
+			{
+				type: "siblings",
+				entities: ["Apollon", "Artémis", "Arès", "Hermès", "Dionysos"],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/Ath%C3%A9na"],
+	},
+	{
+		id: 5,
+		name: "Apollon",
+		pantheon: "Grec",
+		domains: [
+			"soleil",
+			"musique",
+			"poésie",
+			"arts",
+			"prophétie",
+			"guérison",
+			"lumière",
+		],
+		description:
+			"Apollon est le dieu du soleil, de la musique, de la poésie et de la prophétie. Fils de Zeus et de Léto, frère jumeau d'Artémis, il est l'un des dieux les plus vénérés du panthéon grec et préside à l'oracle de Delphes.",
+		symbols: ["lyre", "laurier", "corbeau", "arc", "soleil"],
+		attributes: ["lyre", "arc d'argent", "laurier"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Zeus", "Léto"],
+			},
+			{
+				type: "twins",
+				entities: ["Artémis"],
+			},
+			{
+				type: "siblings",
+				entities: ["Athéna", "Arès", "Hermès", "Dionysos"],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/Apollon"],
+	},
+	{
+		id: 6,
+		name: "Artémis",
+		pantheon: "Grec",
+		domains: [
+			"chasse",
+			"lune",
+			"nature",
+			"animaux sauvages",
+			"chasteté",
+			"accouchement",
+		],
+		description:
+			"Artémis est la déesse de la chasse, de la lune et de la nature sauvage. Fille de Zeus et de Léto, sœur jumelle d'Apollon, elle parcourt les forêts avec ses nymphes et est la protectrice des animaux et des jeunes filles.",
+		symbols: ["arc", "lune", "cerf", "cyprès"],
+		attributes: ["arc d'or", "carquois", "croissant de lune"],
+		relations: [
+			{
+				type: "parents",
+				entities: ["Zeus", "Léto"],
+			},
+			{
+				type: "twins",
+				entities: ["Apollon"],
+			},
+			{
+				type: "siblings",
+				entities: ["Athéna", "Arès", "Hermès", "Dionysos"],
+			},
+		],
+		sources: ["https://fr.wikipedia.org/wiki/Art%C3%A9mis"],
+	},
+];
