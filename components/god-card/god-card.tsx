0c8bdf9868053199468735e1e124256835371383
Mickael <mickaeldesclaux@gmail.com>
2026-03-22 18:13:03 +0100

    add god detail basic page saying hello

--- components/god-card/god-card.tsx
+++ components/god-card/god-card.tsx
@@ -1,5 +1,6 @@
 import { DOMAIN_CLASSES } from "@/components/god-card/domain-tw-classes";
 import { GodPreview, PrimaryDomain } from "@/types/gods";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 type GodCardProps = {
@@ -10,19 +11,24 @@ export default function GodCard({ god }: GodCardProps) {
 	const styles = DOMAIN_CLASSES[god.primaryDomain];
 
 	return (
-		<View
-			className={`bg-surface-2 px-2 py-4 flex-row items-center rounded-card my-2 border ${styles.card}`}
-		>
-			<Text
-				className={`text-3xl p-4 border-[0.5px] ${styles.icon} rounded-2xl`}
+		<Link href={{ pathname: "/god/[id]", params: { id: god.id } }}>
+			<View
+				className={`bg-surface-2 px-2 py-4 flex-row items-center rounded-card my-2 border ${styles.card}`}
 			>
-				{god.icon}
-			</Text>
-			<View className="flex ms-4">
-				<Text className="text-primary text-xl font-cinzel">{god.name}</Text>
-				<CardFooter mainTags={god.mainTags} primaryDomain={god.primaryDomain} />
+				<Text
+					className={`text-3xl p-4 border-[0.5px] ${styles.icon} rounded-2xl`}
+				>
+					{god.icon}
+				</Text>
+				<View className="flex ms-4">
+					<Text className="text-primary text-xl font-cinzel">{god.name}</Text>
+					<CardFooter
+						mainTags={god.mainTags}
+						primaryDomain={god.primaryDomain}
+					/>
+				</View>
 			</View>
-		</View>
+		</Link>
 	);
 }
 
